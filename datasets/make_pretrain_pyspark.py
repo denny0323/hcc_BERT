6ee40a33c16ecf87c0ddd92f690b203646528749
SuminKim <denny0323@gmail.com>
2024-01-19 09:30:35 +0900

    Update make_pretrain_pyspark.py

--- datasets/make_pretrain_pyspark.py
+++ datasets/make_pretrain_pyspark.py
@@ -20,7 +20,7 @@ hc, sc, ss = hs.hive_context, hs.spark_context, hs.spark_session
 check_hive_available(hc)
 
 
-# 예제로 대체 - 데이터 구조 및 스키마에 따라 다름
+# masking - 사내 데이터 구조 및 스키마
 df_dict = {
   'evnt_cd1' : 'col1',
   'evnt_cd2' : ['col1', 'col2'],
@@ -38,6 +38,47 @@ from pyspark.sql import DataFrame
 from pyspark.sql.types import *
 from pyspark.sql.functions import udf
 
+
+df_list = []
+
+START_YRMN = '202201'
+END_YRMN = '202306'
+
+for cd, col in df_dict.items():
+  print(cd, col)
+  if cd in ['evnt_cd3', 'evnt_cd11', 'evnt_cd4', 'evnt_cd7']:
+    df = hc.sql(f"""
+        SELECT a.column1, a.column2, 
+          CASE
+            WHEN a.column3 IS NOT NULL THEN a.column3
+            WHEN a.column3 IS NULL THEN '000000'
+          END AS a.column3, a.{col} AS evnt,
+          a.column5
+        FROM {db_name}{tbl_name} AS a
+        WHERE a.evnt_cd = '{cd}' and a.part_yrmn BETWEEN '{START_YRMN}' and '{END_YRMN}'
+    """)
+
+
+  elif cd == 'evnt_cd2':
+    df = hc.sql(f"""
+      SELECT a.column1, a.column2,
+        CASE
+          WHEN a.column3 IS NOT NULL THEN a.column3
+          WHEN a.column3 IS NULL THEN '000000'
+        END AS a.column3,
+        CASE
+          WHEN (a.column4 != 1111) OR ((a.column4 =  1111) AND (a.{col[1]} = "-")) THEN a.{col[0]}
+          WHEN ((a.column4 = 1111) AND (a.{col[1]} != "-")) THEN a.{col[1]}
+        END AS evnt,
+        a.column5
+      FROM {db_name}{tbl_name} AS a
+        LEFT JOIN {db_name1}{tbl_name1} AS b ON a.{col[1]} = b.column1
+          and b.column2 = '9999-12-31' and b.column2 = '2222'
+        LEFT JOIN {db_name}{tbl_name2} t ON a.column5 = t.column1
+      WHERE a.column5 ='{cd}' and a.part_yrmn BETWEEN '{START_YRMN}' and '{END_YRMN}'
+    """)
+
+  
 
 
 
